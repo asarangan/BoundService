41e4510e75459f2ced808d16fc37ced88b3ca5b0
Andrew Sarangan <asarangan@gmail.com>
2022-06-12 19:25:32 -0400

    All

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,34 +17,31 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 val TAG:String = "TestingService"
-val CHANNEL_ID = "ChannelID"
 
 class MainActivity : AppCompatActivity() {
 
     private var myService: MyService? = null
 
-    private val serviceConnection:ServiceConnection = object : ServiceConnection {
+    private val serviceConnection: ServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
-           myService = (p1 as MyService.MyServiceBinder).getService()
-            Log.d(TAG,"onService Connected")
+            myService = (p1 as MyService.MyServiceBinder).getService()
+            Log.d(TAG, "onService Connected")
         }
 
         override fun onServiceDisconnected(p0: ComponentName?) {
-            Log.d(TAG,"onService Disconnected")
-            //bound = false
+            Log.d(TAG, "onService Disconnected")
         }
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tv: TextView = findViewById(R.id.textView)
+        //This is the intent used for creating the service
         intent = Intent(this, MyService::class.java)
 
-        if (Build.VERSION.SDK_INT  >= Build.VERSION_CODES.O){
-            Log.d(TAG,"Build verison is greater than O")
-            //If build version is greater than O, then we need a notification when service is running. Otherwise it will get killed
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d(TAG, "Build verison is greater than O")
         }
 
 
@@ -59,72 +56,51 @@ class MainActivity : AppCompatActivity() {
 
         //Bind
         findViewById<Button>(R.id.buttonStartBound).setOnClickListener {
-            if (!isMyServiceRunning(MyService::class.java)){
+            if (!isMyServiceRunning(MyService::class.java)) {
 //                Intent(this, MyService::class.java).also {intent ->
 //                bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)}
-                Log.d(TAG,"Service was not running. Establishing bind.")
-                bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-            }
-            else{
-                Log.d(TAG,"Service was already running. Establishing bind.")
-                bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+                Log.d(TAG, "Service was not running. Establishing bind.")
+                bindService(intent, serviceConnection, BIND_AUTO_CREATE)
+            } else {
+                Log.d(TAG, "Service was already running. Establishing bind.")
+                bindService(intent, serviceConnection, BIND_AUTO_CREATE)
             }
         }
 
         //Unbind
         findViewById<Button>(R.id.buttonStopBound).setOnClickListener {
-            if (isMyServiceRunning(MyService::class.java)){
-                    unbindService(serviceConnection)
-                    Log.d(TAG, "Service was running. Unbinding.")
-                    //bound = false
-            }
-            else{
-                Log.d(TAG,"Service was not running. Cannot unbind.")
+            if (isMyServiceRunning(MyService::class.java)) {
+                unbindService(serviceConnection)
+                Log.d(TAG, "Service was running. Unbinding.")
+                //bound = false
+            } else {
+                Log.d(TAG, "Service was not running. Cannot unbind.")
             }
         }
 
         //Start Service
         findViewById<Button>(R.id.buttonStartService).setOnClickListener {
-            if (!isMyServiceRunning(MyService::class.java)){
-                intent = Intent(this, MyService::class.java)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startForegroundService(intent)
-                }
-                Log.d(TAG,"Service was not running. Starting service.")
-            }
-            else{
-                Log.d(TAG,"Service was already running. No need to start again.")
+            if (!isMyServiceRunning(MyService::class.java)) {
+                startService(intent)
+                Log.d(TAG, "Service was not running. Starting service.")
+            } else {
+                Log.d(TAG, "Service was already running. No need to start again.")
             }
         }
 
         //Stop Service
         findViewById<Button>(R.id.buttonStopService).setOnClickListener {
-            if (isMyServiceRunning(MyService::class.java)){
+            if (isMyServiceRunning(MyService::class.java)) {
                 stopService(intent)
-                Log.d(TAG,"Service was running. Stopping service.")
-            }
-            else{
-                Log.d(TAG,"Service was not running. Nothing to stop.")
+                Log.d(TAG, "Service was running. Stopping service.")
+            } else {
+                Log.d(TAG, "Service was not running. Nothing to stop.")
             }
         }
     }
 
 
-//    override fun onStart() {
-//        super.onStart()
-//        // Bind to LocalService
-//        Intent(this, OdometerService::class.java).also { intent ->
-//            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-//        }
-//    }
-
-//    override fun onStop() {
-//        super.onStop()
-//        unbindService(serviceConnection)
-//    }
-
-
-    private fun isMyServiceRunning(serviceClass : Class<*> ) : Boolean{
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
         var manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.name.equals(service.service.className)) {
@@ -133,5 +109,4 @@ class MainActivity : AppCompatActivity() {
         }
         return false
     }
-
 }
